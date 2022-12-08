3ab488b783801693f21b9da4980b83b3bac871a9
Nadezda Andrushko <naduhawww@mail.ru>
2022-12-08 12:39:13 +0700

    решение задачи

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿string[] array = { "1234", "15", "-2", "comp" };
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
+PrintArray(array);
+
+void SortArray(string[] Array)
+{
+    Console.WriteLine("Новый массив:");
+    string[] newArray = new string[Array.Length];
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i].Length <= 3)
+        {
+            newArray[i] = array[i];
+            Console.WriteLine(newArray[i]);
+        }
+    }
+}
+SortArray(array);
+Console.ReadLine();
